60e3b7e591d4a30b394d18b23b4e2389086cbdf1
Burjan Aron <aron.burjan@gmail.com>
2023-03-03 17:34:00 +0100

    Add project files.

---
+++ Models/SolarSystemsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SolarSystems.Models
+{
+    public class SolarSystemsDbContext : DbContext
+    {
+        public SolarSystemsDbContext(DbContextOptions<SolarSystemsDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace SolarSystems.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public int accessLevel { get; set; }
+
+        public User(long id, string name, int accessLevel)
+        {
+            this.Id = Id;
+            this.Name = name;
+            this.accessLevel = accessLevel;
+        }
+
+        public User() { }
+    }
+}
