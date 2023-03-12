ccd925e84060bb09d50cc3582f568ec5f70bb3fc
Broccodile <35729067+Broccodile@users.noreply.github.com>
2023-03-12 01:21:24 +0100

    alap web login

---
+++ Website/LoginPage.js
@@ -0,0 +1,45 @@
+function validateLogin(){
+	
+	//get values from html page
+	const form = {
+		username: document.getElementById("usernameInput"),
+		password: document.getElementById("passwordInput"),
+		submit: document.querySelector("loginButton"),
+	};
+	
+	
+	console.log(JSON.stringify({
+		username: form.username.value,
+		password: form.password.value}))
+	
+	/*
+	
+	//address where requests are sent
+	const loginServer = "";
+	
+	//send request
+	fetch(loginServer, {
+		method: "POST",
+		body: JSON.stringify({
+			email: form.username.value,
+			password: form.password.value,
+		}),
+	})
+    .then(response => {
+		console.log('response.status: ', response.status);
+		console.log(response);
+		if (!response.ok) {
+			console.log('Response not ok');
+			//todo
+		} else {
+		window.open(
+			//redirect to next site
+          "target.html"
+		}
+    }) 
+    .catch((err) => {
+      console.log(err);
+    });	
+	
+	*/
+}
