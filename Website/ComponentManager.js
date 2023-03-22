2a1f45d7f7b48f18c4738b3ed128ab5ac83b8bfa
Broccodile <35729067+Broccodile@users.noreply.github.com>
2023-03-22 17:44:18 +0100

    update ComponentManager
    Add/Edit components now fully working

--- Website/ComponentManager.js
+++ Website/ComponentManager.js
@@ -8,42 +8,85 @@
           // Edit mode
           addMode.style.display = "none";
           editMode.style.display = "block";
-          form.setAttribute("onsubmit", "event.preventDefault(); editComponent()");
+          //form.setAttribute("onsubmit", "event.preventDefault(); editComponent()");
 		  document.getElementById("ModeLabel").innerHTML = "Edit";
         } else {
           // Add mode
           addMode.style.display = "block";
           editMode.style.display = "none";
-          form.setAttribute("onsubmit", "event.preventDefault(); addComponent()");
+          //form.setAttribute("onsubmit", "event.preventDefault(); addComponent()");
 		  document.getElementById("ModeLabel").innerHTML = "Add";
         }
       }
 
-      function addComponent() {
+    function addComponent() {
         const name = document.getElementById("nameInput").value;
         const price = document.getElementById("priceInput").value;
         const max = document.getElementById("maxInput").value;
-        console.log(JSON.stringify({
-		name: name,
-		price: price,
-		max: max}))
+		const address = "https://localhost:7032/api/Components"
+
         console.log("Adding component:", name, price, max);
-		//TODO: send request
-		updateTable();
-      }
+		fetch(address, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				componentName: name,
+				maxQuantity: max,
+				price: price})
+		})
+		.then(response => {
+			console.log('response.status: ', response.status);
+			console.log(response);
+			if (!response.ok) {
+				console.log('Response not ok');
+				document.getElementById("errorMessage").innerHTML = "ERROR: " + response.status;
+			} 
+		})
+		//updateTable();
+	}
 
       function editComponent() {
         const id = document.getElementById("idInput").value;
         const price = document.getElementById("newPriceInput").value;
+		const address = "https://localhost:7032/api/Components/" + id + "/" + price;
         console.log("Editing component:", id, price);
-		//TODO: send request
-		updateTable();
+		fetch(address, {
+			method: "PUT"
+		})
+		.then(response => {
+			console.log('response.status: ', response.status);
+			console.log(response);
+			if (!response.ok) {
+				console.log('Response not ok');
+				document.getElementById("errorMessage").innerHTML = "ERROR: " + response.status;
+			} 
+		})
+		//updateTable();
 	  }
 	  
-	  function updateTable(){
-			addTableRow(0);
-	  }
-	  
+	 function updateTable(){
+			let address = "https://localhost:7032/api/Components";
+			fetch(address, {
+				method: "GET"
+			})
+			.then(response => {
+				currentStatus = response.status;
+				console.log('response.status: ', response.status);
+				if (response.ok){
+					//add data to table
+					//TODO: add actual data
+					//console.log(JSON.stringify(response.json));
+					
+				}
+			})
+
+			.catch((err) => {
+				console.log(err);
+			});	
+	}
+	  /*
 	  function addTableRow(currentID){
 		let address = "https://localhost:7032/api/Components/"
 		let currentStatus=0;
@@ -62,8 +105,7 @@
 					let cellPrice = row.insertCell(2);
 					let cellMax = row.insertCell(3);
 					//TODO: add actual data
-					cellID.innerHTML = "testID";
-					cellName.innerHTML = "testName";
+					
 					addTableRow(currentID+1);
 				}
 			})
@@ -72,4 +114,4 @@
 			});	
 			console.log(currentStatus)
 	  }
-	  
+	  */
