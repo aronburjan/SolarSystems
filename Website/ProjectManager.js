b806b86a9943794174415883fc5f56bd90729241
gtgunar <120199255+gtgunar@users.noreply.github.com>
2023-05-26 22:59:40 +0200

    Update ProjectManager.js

--- Website/ProjectManager.js
+++ Website/ProjectManager.js
@@ -69,15 +69,17 @@
 					
 					let row 				= table.insertRow(counter);
 					
-					let cellPlace			= row.insertCell(0);
-					let cellCustomer		= row.insertCell(1);
-					let cellDescription 	= row.insertCell(2);
-					let cellLtime 			= row.insertCell(3);
-					let cellLrate 			= row.insertCell(4);
-					let cellStatus = row.insertCell(5);
-					let cellTotalPrice = row.insertCell(6);
+					let cellID 				= row.insertCell(0);
+					let cellPlace			= row.insertCell(1);
+					let cellCustomer		= row.insertCell(2);
+					let cellDescription 	= row.insertCell(3);
+					let cellLtime 			= row.insertCell(4);
+					let cellLrate 			= row.insertCell(5);
+					let cellStatus = row.insertCell(6);
+					let cellTotalPrice = row.insertCell(7);
 					
 					//fill out cell data
+					cellID.innerHTML 			= item.id;
 					cellPlace.innerHTML 		= item.projectLocation;
 					cellCustomer.innerHTML 		= item.customerName;
 					cellDescription.innerHTML 	= item.projectDescription;
@@ -85,6 +87,9 @@
 					cellLrate.innerHTML 		= item.hourlyLaborRate;
 					cellStatus.innerHTML = item.currentStatus;
 					cellTotalPrice.innerHTML = item.totalPrice;
+					//add event listener for selection
+					row.addEventListener("click", () => selectRow(row));
+					
 					counter++;
 					}); 
 				})
@@ -93,3 +98,96 @@
 			});	
 		}
 	
+
+function selectRow(r){
+	const table = document.getElementById("projectTable");
+    const rows = table.getElementsByTagName("tr");
+	
+	for (let i = 0; i < rows.length; i++) {
+          rows[i].classList.remove("selected");
+    }
+	console.log(r);
+	r.classList.add("selected")
+}
+
+function getSelectedID() {
+  const table = document.getElementById("projectTable");
+  const rows = table.getElementsByTagName("tr");
+  
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].classList.contains("selected")) {
+      const cells = rows[i].getElementsByTagName("td");
+      return cells[0].innerHTML;
+    }
+  }
+  
+  return null; // Return null if no selected row
+}
+
+function doPriceCalc(){
+	const projectID = getSelectedID();
+	const address = "https://localhost:7032/api/Projects/" + projectID;
+	
+	fetch(address)
+		.then(response => response.json())
+		.then(data => {
+			if (data.currentStatus != "Draft") {
+				console.log("selected project is not Draft (or no project is selected)");
+				document.getElementById("errorMessage").innerHTML = "To do price calculation, select a project that is in Draft status!";
+			} else {
+				
+				const estimateAddress = "https://localhost:7032/estimate/" + projectID;
+
+				fetch(estimateAddress, {
+					method: 'GET'
+				})
+				.then(() => {
+					console.log("Price estimated for project " + projectID);
+					updateTable();
+				})
+				.catch(error => {
+					console.error('Error:', error);
+				});
+				
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+}
+
+function closeProject(status){
+	const projectID = getSelectedID();
+	const address = "https://localhost:7032/api/Projects/" + projectID;
+	
+	fetch(address)
+		.then(response => response.json())
+		.then(data => {
+			if (data.currentStatus != "InProgress") {
+				console.log("selected project is not InProgress (or no project is selected)");
+				document.getElementById("errorMessage").innerHTML = "To close a project, select a project that is in InProgress status!";
+			} else {
+				
+				if((status == "Completed") || (status=="Failed")){
+					const closeAddress = "https://localhost:7032/api/Projects/set/status/" + projectID + "/" + status;
+					fetch(closeAddress, {
+						method: 'PUT'
+					})
+					.then(() => {
+						console.log("Closed project " + projectID + " with status " + status);
+						updateTable();
+					})
+					.catch(error => {
+						console.error('Error:', error);
+					});
+					
+				}
+				
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+		
+
+}
